59797564cfe36ee87e577adf8df09f30b6c4b880
tomlegge <tom.legge@gmail.com>
2020-06-24 10:05:24 +0100

    improved comments

--- anydice.py
+++ anydice.py
@@ -5,43 +5,48 @@ Created on Thu Jun 11 16:50:00 2020
 @author: Tom Legge
 """
 # Sound credits
-## This program uses sound https://freesound.org/people/LunarThePunar/sounds/322030/ by user LunarThePunar under a creative commons licence 
+## This program uses sound from https://freesound.org/people/LunarThePunar/sounds/322030/ by user LunarThePunar under a creative commons licence &
+## This program uses sound from https://freesound.org/people/dylanperitz/sounds/444186/ by the user dylanperitz
 
-# Import libraries needed for this programme
-import random, dicenumbers
+# Import libraries needed for this program
+import random
+## dicenumbers is used to generate the 'images' of the dice
+import dicenumbers
 from time import sleep
 from playsound import playsound
 
-# Set the time delay between each message
+# Set the time delay between each message, in seconds
 delay = 0.78
 
+# most of the code is inside the function 'start' so that it can be re-run without having to restart the program
 def start():
-    # Getting the user input and converting it to an integer
+    # Asking the player how many sides they want on their dice and converting it to an integer
     n = int(input('\nHow many sides to you want on your dice?: '))
     
-    # Does the user have a rolling tray? This reduces the probability of the dice rolling off the table
+    # Does the user have a rolling tray? This reduces the probability of the dice rolling off the table. The variable 'prop' us used later as input in a random number generator
     tray = input('\nAre you going to use a dice tray? Answer Y or N: ')
     if tray == 'Y':
         prob = 1000000
     else:
         prob = 10
     
-    # Generate some random numbers, taking the user input as the maximum number.
+    # Generate some random numbers, taking the player input 'n' as the maximum possible random number.
     rolling1 = int(random.randint(1, n))
     rolling2 = int(random.randint(1, n))
     rolling3 = int(random.randint(1, n))
-    final_string = int(random.randint(1, n))
+    final_number = int(random.randint(1, n))
     
-    # Generate a random number to see if the dice falls off the table. Highest the second number, lower the probability of the dice falling off the table
+    # Generate a random number to see if the dice falls off the table. The variable 'prob' is used for the highest possible number
     falloff = int(random.randint(1, prob))
     
     # Setting a boolen variable to equal False - if the dice falls off the table, this is changed to True
     felloff  = bool(False)
-    # Play some dramatic music
+    
+    # Play some dramatic music once the dice starts rolling and a sound effect of a dice being shaken and rolled. Allow these to play simultaneously
     playsound('322030__lunarthepunar__tension-building-intro.wav', block = False)
     playsound('444186__dylanperitz__shaking-and-rolling-dice.wav', block = False)
     
-    # take the random number variables and match them to a number animation
+    # take the random number variables and match them to a number animation - this shows the progress of the dice as it rolls across the table - or in some cases, falls off
     print("\nYou roll your dice..\n"); sleep(delay)
     
     if falloff ==1:
@@ -151,34 +156,34 @@ def start():
     else:
         print("\nBut it is still rolling..\n"); sleep(delay)
         print("\nAaah, finally -it has stopped and your dice landed on: \n"); sleep(delay*2)
-        if final_string ==1: 
+        if final_number ==1: 
             dicenumbers.print_one(); sleep(delay)
         else:
-            if final_string ==2:
+            if final_number ==2:
                 dicenumbers.print_two(); sleep(delay)
             else:
-                if final_string ==3:
+                if final_number ==3:
                     dicenumbers.print_three(); sleep(delay)
                 else:
-                    if final_string ==4:
+                    if final_number ==4:
                         dicenumbers.print_four(); sleep(delay)
                     else:
-                        if final_string ==5:
+                        if final_number ==5:
                             dicenumbers.print_five(); sleep(delay)
                         else:
-                            if final_string ==6:
+                            if final_number ==6:
                                 dicenumbers.print_six(); sleep(delay)
                             else:
-                                if final_string ==7:
+                                if final_number ==7:
                                     dicenumbers.print_seven(); sleep(delay)        
                                 else:
-                                    if final_string ==8:
+                                    if final_number ==8:
                                         dicenumbers.print_eight(); sleep(delay)
                                     else:
-                                        if final_string ==9:
+                                        if final_number ==9:
                                             dicenumbers.print_nine(); sleep(delay)
                                         else:
-                                            print(final_string); sleep(delay)
+                                            print(final_number); sleep(delay)
 
 
 restart = bool(True)
